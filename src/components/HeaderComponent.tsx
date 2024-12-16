26ba58dc03f320e9c1c1a4a3e8d8722b3863689e
AyoLaww <104886347+AyoLaww@users.noreply.github.com>
2024-12-16 13:01:00 +0300

    Changed bio

--- src/components/HeaderComponent.tsx
+++ src/components/HeaderComponent.tsx
@@ -13,8 +13,8 @@ export function HeaderComponent(){
               className="w-20 md:w-20 lg:w-24"
               />
 
-              <h1 className="text-lg  lg:text-3xl font-bold opacity-85 py-5">Im Law -- A web designer and developer</h1>
-              <p className="opacity-80">I am currently a university student majoring in computer science. I have a burning passion for tech and entrepreneurship, I am currently working on a few exciting projects like <a href="https://x.com/NearStudios" className="text-sky-500 hover:underline" target="_blank">@NearStudios</a>, a web design and development studio, and learning cybersecurity on the side.</p>
+              <h1 className="text-lg  lg:text-3xl font-bold opacity-85 py-5">Hi, Im Law.</h1>
+              <p className="opacity-80">A computer science major at USIU-A. Apart from slaving away at school work involuntarily in school I am also slaving away at school work voluntarily at home where I teach myself cybersecurity🥲. I also run <a href="https://nearstudios.framer.website" className="text-sky-500 hover:underline" target="_blank">Near studios</a>, a web design and development agency that helps SMEs and individuals build their online presence, oh and I also love cars...a lot.</p>
           </div>
           
 
